ea57b451dbb40ce9f0a30b640eae370774528d9d
Maxwell Ward <max.jp.ward@gmail.com>
2025-12-05 01:11:22 -0800

    improve PingResponse types

--- src/types/ping.ts
+++ src/types/ping.ts
@@ -2,5 +2,5 @@ import type { PorkbunBaseResponse } from "../client";
 
 export interface PingResponse extends PorkbunBaseResponse {
 	yourIp: string;
-	xForwardedFor: string;
+	xForwardedFor?: string;
 }
